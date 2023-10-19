1dd6fae05f06e51a9d45f876a70d4f1c472a3d62
BluesUnity <93047050+BluesUnity@users.noreply.github.com>
2023-10-19 15:52:52 +0200

    Update and rename ldap.s to ldap.sh

--- ldap.sh
+++ ldap.sh
@@ -2,6 +2,7 @@
 
 apt update
 apt install slapd ldap-utils
+apt install rsyslog
 
 dpkg-reconfigure slapd
 
@@ -24,9 +25,35 @@ replace: olcLogLevel
 olcLogLevel: conns filter config ACL stats stats2" >> slapdlog.ldif
 
 ldapmodify -Y external -H ldapi:/// -f slapdlog.ldif
+
+
 echo "local4.* /var/log/slapd.log" >> /etc/rsyslog.d/10-ldap.conf
 
 systemctl restart rsyslog
-
+echo "fin rsyslog"
 ldapsearch -x -H ldap://localhost -D cn=admin,dc=$domaine,dc=private -W -b cn=config
+
+touch /etc/ldap/access-admin.ldif
+echo "
+dn: olcDatabase={0}config,cn=config
+changeType: modify
+add: olcAccess
+olcAccess: to * by dn.exact=cn=admin,dc=mondomaine,dc=private manage by * break
+" >> /etc/ldap/acces-admin.ldif
+
+ldapmodify -Y external -H ldapi:/// -f /etc/ldap/acces-admin.ldif
+
+ldapsearch -x -H ldap://localhost -D cn=admin,dc=$domaine,dc=private -W -b cn=
+config
+
+ldapsearch -x -s one -H ldap://localhost -D cn=admin,dc=$domaine,dc=private -W
+-b cn=schema,cn=config cn -LLL
+echo "fin accessadmin"
+touch /etc/ldap/ppolicy-module.ldif
+echo "
+dn: cn=module{0},cn=config
+changeType: modify
+add: olcModuleLoad
+olcModuleLoad: ppolicy" >> /etc/ldap/ppolicy-module.ldif
+
 
